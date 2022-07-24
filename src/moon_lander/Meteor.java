17ef561a875bc468586547f68ecbf858e3f12587
dvpaa <kgo0926@naver.com>
2022-07-24 16:42:39 +0900

    edit annotation

---
+++ src/moon_lander/Meteor.java
@@ -0,0 +1,22 @@
+package moon_lander;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+/**
+ * ItemS which player will have to get
+ *
+ * @author https://github.com/dvpaa/Java-2022-Summer
+ */
+
+public class Meteor {
+
+}
